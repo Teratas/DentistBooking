6d2b918915d656300256bd30bb52be5e8b5e60c4
Teratas <tienyuop@gmail.com>
2024-11-15 00:23:01 +0700

    vercel

--- src/app/(main)/main/page.tsx
+++ src/app/(main)/main/page.tsx
@@ -2,8 +2,8 @@ import MainBanner from "@/components/MainBanner";
 import { Suspense } from "react";
 import { CircularProgress } from "@mui/material";
 export default function MainPage() {
-    
 
+    
     return (
         <main className='text-black'>
             <div className={`z-0 w-[100vw] h-[100vh] z-10 top-0 absolute bg-gradient-to-l from-teal-300 to-stone-300`}>
