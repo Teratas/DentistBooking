d6ebf8771ac0de776cffb94f85e7bb090e719b2a
Teratas <tienyuop@gmail.com>
2024-11-14 22:20:01 +0700

    VercalFix2

--- src/app/(main)/main/page.tsx
+++ src/app/(main)/main/page.tsx
@@ -1,5 +1,4 @@
 import MainBanner from "@/components/MainBanner";
-import MainSlideBar from "@/components/MainSlideBar";
 import { Suspense } from "react";
 import { CircularProgress } from "@mui/material";
 export default function MainPage() {
