43cfb8b03e39b6d218f5dc38522a676594386c35
Teratas <tienyuop@gmail.com>
2024-11-16 23:04:54 +0700

    fix middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,5 +1,5 @@
 export {default} from 'next-auth/middleware'
 
 export const config = {
-    matcher : ["/", '/main','/myBooking','/myProfile', '/dentistPage']
+    matcher : ['/myBooking','/myProfile', '/dentistPage']
 }
